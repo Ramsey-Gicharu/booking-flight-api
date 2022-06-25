a322ae7abaf9899184d20cf21b28f98338f21832
Ramsey Gicharu <89084952+Ramsey-Gicharu@users.noreply.github.com>
2022-06-25 17:43:53 +0300

    deleted  fligght.js and created flight.json

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const express = require("express");
-const { json } = require("express");
-const flights = require("./controllers/flightController");
-const models = require("./models/Flight");
-const routes = require("./routes/flightRoute");
 
-const app = express();
+const app =  express();
 
-app.use(json());
+const PORT = process.env.PORT || 3000;
 
-app.use("/", routes);
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get("/",(req, res)=>{
+  res.send("Zuri Training");
 });
+
+app.listen(PORT, () => console.log('Serving on port ${PORT}'));
