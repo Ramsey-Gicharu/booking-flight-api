a57745962543fd63e152b84c33cac9a14db9bb54
Ramsey Gicharu <89084952+Ramsey-Gicharu@users.noreply.github.com>
2022-07-02 13:56:50 +0300

    edits

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,6 +1,10 @@
-exports.example = (req, res) => {
-    console.log("example")
-    res.send("Flight example")
+const Flights = require("./models/flight");
+
+exports.getFlights() = asynch (req, res) => {
+    try{
+        const flights = await.flights  
+        res.stautus(200).json(flights);
+    }catch (err){
+        res.status(500).json({message: err})
+    }
 }
-
-
